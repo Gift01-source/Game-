fa7613c69602e7b960fb5f6351940391848c5c1f
Game <bae23-gchimwaza@poly.ac.mw>
2024-11-25 14:39:31 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
+//game.js
+class Game extends Phaser.scene
+  {
+    constructor(){
+      supper('Game');
+     }
+    preload(){
+      //Load game assets here
+     }
+    create(){
+//Create game objects here
+    }
+    update(time, delta) {
+      //Update game logic here
+    }
+  }
 
+const config={
+type=Phaser.AUTO,
+  width:800,
+  height:600,
+  scene: [Game]
+  };
+
+const game=new
+  Phaser.Game(config);
